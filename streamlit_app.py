acad8886f74802255894f03d50eae8ede35c3f7f
BiserkaUgre <146072341+BiserkaUgre@users.noreply.github.com>
2023-09-29 13:25:38 +0200

    Update streamlit_app.py
    corrected ' in the line 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner)
-streamlit.header('Breakfast Manu')
-strealit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-Ranged Eggs')
+streamlit.title('My Parents New Healthy Dinner')
+
